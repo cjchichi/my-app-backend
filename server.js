5873983ae6876b6552bbc77f02f94d77c1ec3ed5
cjchichi <cynthia1973chichi@gmail.com>
2025-03-31 01:25:24 +0300

    add server.js

--- server.js
+++ server.js
@@ -1,4 +1,4 @@
-/*
+
 const jsonServer = require('json-server')
 
 
@@ -17,32 +17,6 @@ server.use((req, res, next) => {
 server.listen(process.env.PORT || 5000, () => {
     console.log('JSON Server is running')
 })
-*/
 
-const jsonServer = require('json-server');
 
-const server = jsonServer.create();
-const router = jsonServer.router('db.json');
-const middlewares = jsonServer.defaults();
 
-// Middlewares
-server.use(middlewares);
-
-// Handle CORS and Preflight Requests
-server.use((req, res, next) => {
-    res.header('Access-Control-Allow-Origin', 'https://cjchichi.github.io/End-Phase-1-Project'); // Allow frontend URL
-    res.header('Access-Control-Allow-Headers', 'Origin, X-Requested-With, Content-Type, Accept');
-    res.header('Access-Control-Allow-Methods', 'GET, POST, PUT, DELETE, OPTIONS');
-    if (req.method === 'OPTIONS') {
-        return res.status(200).end(); // Respond to preflight requests
-    }
-    next();
-});
-
-// Use the JSON Router
-server.use('/api', router);
-
-// Start Server
-server.listen(process.env.PORT || 5000, () => {
-    console.log('JSON Server is running');
-});
