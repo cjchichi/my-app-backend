b32c737bb40b9a4e2a8fcb254d2f29104b0ac114
cjchichi <cynthia1973chichi@gmail.com>
2025-03-31 01:08:57 +0300

    add new file:   .vscode/settings.json         modified:   server.js

--- server.js
+++ server.js
@@ -1,3 +1,4 @@
+/*
 const jsonServer = require('json-server')
 
 
@@ -9,10 +10,39 @@ const middlewares = jsonServer.defaults()
 server.use(middlewares)
 server.use('/api', router)
 server.use((req, res, next) => {
-    res.header('Access-Control-Allow-Origin', 'https://cjchichi.github.io/End-Phase-1-Project/') // The URL you put here is for the web application that you have deployed using Github Pages
+    res.header('Access-Control-Allow-Origin', 'https://cjchichi.github.io/End-Phase-1-Project') // The URL you put here is for the web application that you have deployed using Github Pages
     res.header('Access-Control-Allow-Headers', '*')
     next()
 })
-server.listen(process.env.PORT || 5500, () => {
+server.listen(process.env.PORT || 5000, () => {
     console.log('JSON Server is running')
 })
+*/
+
+const jsonServer = require('json-server');
+
+const server = jsonServer.create();
+const router = jsonServer.router('db.json');
+const middlewares = jsonServer.defaults();
+
+// Middlewares
+server.use(middlewares);
+
+// Handle CORS and Preflight Requests
+server.use((req, res, next) => {
+    res.header('Access-Control-Allow-Origin', 'https://cjchichi.github.io/End-Phase-1-Project'); // Allow frontend URL
+    res.header('Access-Control-Allow-Headers', 'Origin, X-Requested-With, Content-Type, Accept');
+    res.header('Access-Control-Allow-Methods', 'GET, POST, PUT, DELETE, OPTIONS');
+    if (req.method === 'OPTIONS') {
+        return res.status(200).end(); // Respond to preflight requests
+    }
+    next();
+});
+
+// Use the JSON Router
+server.use('/api', router);
+
+// Start Server
+server.listen(process.env.PORT || 5000, () => {
+    console.log('JSON Server is running');
+});
